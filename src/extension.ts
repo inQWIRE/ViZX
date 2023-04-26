87b9cd05e47c1123bb27b31b581fc00b62e66085
BHAKTISHAH <shahbhakti21@gmail.com>
2023-04-25 23:29:50 -0500

    version bump 0.0.4: changed lsp extraction argument, added sizing and deactivation commands, minor parsing bug

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,14 @@ import * as vscode from "vscode";
 import * as parser from "./parsing/parser";
 import * as sizer from "./parsing/sizes";
 import * as coord from "./parsing/coords";
-import { boundary, setCanvasWidthHeight } from "./constants/consts";
+import { boundary, setCanvasWidthHeight, scaleUp, scaleDown } from "./constants/consts";
+import * as consts from "./constants/consts";
 import * as ast from "./parsing/ast";
 import { getCanvasHtml } from "./webview/webview";
+
+let openTabNames : String[] = [];
+let openWebview : vscode.WebviewPanel | undefined = undefined;
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -32,6 +37,23 @@ export function activate(context: vscode.ExtensionContext) {
     );
   });
   context.subscriptions.push(disposable);
+  disposable = vscode.commands.registerCommand("vizx.deactivateRendering", () => {
+    deactivate();
+    (vscode.window.showInformationMessage(
+      "Automatic rendering is now turned off."
+    ));
+  });
+  context.subscriptions.push(disposable);
+  disposable = vscode.commands.registerCommand("vizx.scaleUp", () => {
+    scaleUp();
+    console.log("SCALE: ", consts.SCALE, " HEIGHT: ", consts.CANVAS_HEIGHT, "WIDTH: ", consts.CANVAS_WIDTH)
+  });
+  context.subscriptions.push(disposable);
+  disposable = vscode.commands.registerCommand("vizx.scaleDown", () => {
+    scaleDown();
+    console.log("SCALE: ", consts.SCALE, " HEIGHT: ", consts.CANVAS_HEIGHT, "WIDTH: ", consts.CANVAS_WIDTH)
+  });
+  context.subscriptions.push(disposable);
 }
 
 function renderCallback(context: vscode.ExtensionContext, expr: any) {
@@ -41,14 +63,20 @@ function renderCallback(context: vscode.ExtensionContext, expr: any) {
       return;
     }
     if (expr.arg1 !== undefined) {
-      expr = expr.arg1.goals.goals[0].ty;
+      // extract correct field from lsp information
+      expr = expr.goals.goals[0].ty;
     }
     console.log("expr: ", expr);
+    if(openTabNames.includes(expr)) {
+      return;
+    }
+    openTabNames.push(expr);
     let node: ast.ASTNode;
     try {
       node = parser.parseAST(expr);
       node = sizer.addSizes(node);
       const size = sizer.determineCanvasWidthHeight(node);
+      console.log("size = ", size);
       setCanvasWidthHeight(size);
       node = coord.addCoords(node, boundary);
     } catch (e) {
@@ -57,15 +85,19 @@ function renderCallback(context: vscode.ExtensionContext, expr: any) {
       );
       return;
     }
+    if (openWebview !== undefined) {
+      openWebview.dispose();
+    }
     const panel = vscode.window.createWebviewPanel(
       "ViZX",
       `ViZX: ${expr}`,
-      vscode.ViewColumn.One,
+      vscode.ViewColumn.Three,
       {
         enableScripts: true,
         retainContextWhenHidden: true,
       }
     );
+    openWebview = panel;
     panel.webview.html = getCanvasHtml(panel, context);
     panel.webview.onDidReceiveMessage((msg) => console.log(msg));
     panel.webview.postMessage({ command: JSON.stringify(node) });
